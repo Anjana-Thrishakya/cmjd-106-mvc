cafc772fcf7483a70a86c15668e5d82fa8e1f5ba
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-06-15 13:05:01 +0530

    Place Order Transaction

--- src/edu/ijse/mvc/controller/OrderController.java
+++ src/edu/ijse/mvc/controller/OrderController.java
@@ -6,6 +6,7 @@ package edu.ijse.mvc.controller;
 
 import edu.ijse.mvc.dto.OrderDetailDto;
 import edu.ijse.mvc.dto.OrderDto;
+import edu.ijse.mvc.model.OrderModel;
 import java.util.ArrayList;
 
 /**
@@ -14,7 +15,14 @@ import java.util.ArrayList;
  */
 public class OrderController {
     
+    private OrderModel orderModel;
+
+    public OrderController() throws Exception {
+        orderModel = new OrderModel();
+    }
+    
+    
     public String placeOrder(OrderDto orderDto, ArrayList<OrderDetailDto> orderDetailDtos) throws Exception{
-        return null;
+        return orderModel.placeOrder(orderDto, orderDetailDtos);
     }
 }
